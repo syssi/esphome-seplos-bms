78e7da3176bc87e14d5048b04bfc0b8a9f890f35
Sebastian Muszynski <basti@linkt.de>
2025-11-30 15:08:27 +0100

    Add ESPHome 2025.12 compatibility layer for address_str() API change (#169)

--- components/seplos_bms_ble/seplos_bms_ble.cpp
+++ components/seplos_bms_ble/seplos_bms_ble.cpp
@@ -1,6 +1,13 @@
 #include "seplos_bms_ble.h"
 #include "esphome/core/log.h"
 #include "esphome/core/helpers.h"
+#include "esphome/core/version.h"
+
+#if ESPHOME_VERSION_CODE >= VERSION_CODE(2025, 12, 0)
+#define ADDR_STR(x) x
+#else
+#define ADDR_STR(x) (x).c_str()
+#endif
 
 namespace esphome {
 namespace seplos_bms_ble {
@@ -172,7 +179,8 @@ void SeplosBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
       auto *char_notify =
           this->parent_->get_characteristic(SEPLOS_BMS_SERVICE_UUID, SEPLOS_BMS_NOTIFY_CHARACTERISTIC_UUID);
       if (char_notify == nullptr) {
-        ESP_LOGE(TAG, "[%s] No notify service found at device, not an Seplos v2 BMS..?", this->parent_->address_str());
+        ESP_LOGE(TAG, "[%s] No notify service found at device, not an Seplos v2 BMS..?",
+                 ADDR_STR(ADDR_STR(this->parent_->address_str())));
         break;
       }
       this->char_notify_handle_ = char_notify->handle;
@@ -186,7 +194,8 @@ void SeplosBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
       auto *char_command =
           this->parent_->get_characteristic(SEPLOS_BMS_SERVICE_UUID, SEPLOS_BMS_CONTROL_CHARACTERISTIC_UUID);
       if (char_command == nullptr) {
-        ESP_LOGE(TAG, "[%s] No control service found at device, not an BASEN BMS..?", this->parent_->address_str());
+        ESP_LOGE(TAG, "[%s] No control service found at device, not an BASEN BMS..?",
+                 ADDR_STR(ADDR_STR(this->parent_->address_str())));
         break;
       }
       this->char_command_handle_ = char_command->handle;
@@ -212,7 +221,7 @@ void SeplosBmsBle::gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t
 
 void SeplosBmsBle::update() {
   if (this->node_state != espbt::ClientState::ESTABLISHED) {
-    ESP_LOGW(TAG, "[%s] Not connected", this->parent_->address_str());
+    ESP_LOGW(TAG, "[%s] Not connected", ADDR_STR(ADDR_STR(this->parent_->address_str())));
     return;
   }
 
@@ -1304,7 +1313,8 @@ bool SeplosBmsBle::send_command(uint8_t function, const std::vector<uint8_t> &pa
                                data.size(), &data.front(), ESP_GATT_WRITE_TYPE_NO_RSP, ESP_GATT_AUTH_REQ_NONE);
 
   if (status) {
-    ESP_LOGW(TAG, "[%s] esp_ble_gattc_write_char failed, status=%d", this->parent_->address_str(), status);
+    ESP_LOGW(TAG, "[%s] esp_ble_gattc_write_char failed, status=%d", ADDR_STR(ADDR_STR(this->parent_->address_str())),
+             status);
   }
 
   return (status == 0);
