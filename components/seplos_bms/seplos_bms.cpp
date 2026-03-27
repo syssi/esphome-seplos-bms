dc29755988c65b1dd29ea43a282bf3f7145aafa7
Sebastian Muszynski <basti@linkt.de>
2026-03-27 07:36:12 +0100

    Remove duplicate charging_cycles entry from dump_config
    The charging_cycles sensor was logged twice in SeplosBms::dump_config,
    once at line 193 and again at line 198 after the rated_capacity entry.

--- components/seplos_bms/seplos_bms.cpp
+++ components/seplos_bms/seplos_bms.cpp
@@ -195,7 +195,6 @@ void SeplosBms::dump_config() {
   LOG_SENSOR("", "Residual capacity", this->residual_capacity_sensor_);
   LOG_SENSOR("", "Battery capacity", this->battery_capacity_sensor_);
   LOG_SENSOR("", "Rated capacity", this->rated_capacity_sensor_);
-  LOG_SENSOR("", "Charging cycles", this->charging_cycles_sensor_);
   LOG_SENSOR("", "Average Cell Voltage", this->average_cell_voltage_sensor_);
   LOG_SENSOR("", "State of health", this->state_of_health_sensor_);
   LOG_SENSOR("", "Port Voltage", this->port_voltage_sensor_);
